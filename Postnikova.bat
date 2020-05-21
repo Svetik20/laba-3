7460a07d92016b379172690db29e335d4d28e177
Svetik20 <63389990+Svetik20@users.noreply.github.com>
2020-05-21 12:32:34 +0300

    Add files via upload

---
+++ Postnikova.bat
@@ -0,0 +1,25 @@
+Echo off
+md Vikersen
+cd Vikersen
+md Olaf
+cd Olaf
+md Osteyn
+cd Osteyn
+pause
+echo > Darkthrone.txt
+cd .. 
+echo > 1347.txt
+cd ..
+echo > Nothern.txt
+cd ..
+pause
+del Vikersen /S /Q /F
+pause
+cd Vikersen
+cd Olaf
+rd Osteyn
+cd ..
+rd Olaf
+cd ..
+rd  Vikersen
+pause
